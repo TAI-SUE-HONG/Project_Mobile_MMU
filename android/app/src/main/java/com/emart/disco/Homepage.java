e09690af9a24d1d7878da52bb15682f280d4bd13
Alex-Yen <61192284+Alex-Yen@users.noreply.github.com>
2022-03-15 17:40:04 +0800

    update(15/03)

--- android/app/src/main/java/com/emart/disco/Homepage.java
+++ android/app/src/main/java/com/emart/disco/Homepage.java
@@ -10,5 +10,6 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
     }
 }
