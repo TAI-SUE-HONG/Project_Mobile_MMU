12742aa749edc160eaea668b82277eda037f553e
WongWanKei <97347617+WongWanKei@users.noreply.github.com>
2022-03-17 20:17:32 +0800

    .

---
+++ android/app/src/main/java/com/emart/disco/disco_cart.java
@@ -0,0 +1,14 @@
+package com.emart.disco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class disco_cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disco_cart);
+    }
+}
