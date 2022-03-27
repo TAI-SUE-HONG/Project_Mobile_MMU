089fc3d3aa06840f49ed758430a0269e804a2c0d
WongWanKei <97347617+WongWanKei@users.noreply.github.com>
2022-03-27 20:22:22 +0800

    。

--- android/app/src/main/java/com/emart/disco/disco_cart_adapter.java
+++ android/app/src/main/java/com/emart/disco/disco_cart_adapter.java
@@ -5,12 +5,14 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class disco_cart_adapter {
+public class disco_cart_adapter extends RecyclerView.Adapter<disco_cart_adapter.CartViewHolder>{
     class CartViewHolder extends RecyclerView.ViewHolder
     {
+        private Unbinder unbinder;
 
         public CartViewHolder(@NonNull View itemView) {
             super(itemView);
+            unbinder=
         }
     }
 }
